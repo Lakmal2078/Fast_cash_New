3250702ae60198a5b2bd4c80bc5687e0c1805245
Lakmal Vidana Gamage <lakmalsujith25@gmail.com>
2026-08-02 01:42:10 +0000

    vs code workflow

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fastcash.app',
+  appName: 'Fast Cash',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
