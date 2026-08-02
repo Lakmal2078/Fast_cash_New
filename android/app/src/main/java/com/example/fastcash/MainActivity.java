f04d1059feda717158bcf51fc5fa00fb712df6ed
Lakmal Vidana Gamage <180389474+Lakmal2078@users.noreply.github.com>
2026-08-03 02:55:09 +0530

    Set up WebView to load local HTML content
    Initialize WebView in MainActivity to load local HTML file and enable JavaScript.

--- android/app/src/main/java/com/example/fastcash/MainActivity.java
+++ android/app/src/main/java/com/example/fastcash/MainActivity.java
@@ -1,12 +1,43 @@
 package com.example.fastcash;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
